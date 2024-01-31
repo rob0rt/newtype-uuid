c939e000042730bbfd8e423cf93165773108bd98
sunshowers <rain@sunshowers.io>
2024-01-31 01:56:45 +0000

    Deploying to gh-pages from @ oxidecomputer/newtype-uuid@548be6ece6e9aa249963829c132c112ecaf93b24 🚀

---
+++ rustdoc/trait.impl/schemars/trait.JsonSchema.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"newtype_uuid":[["impl&lt;T&gt; <a class=\"trait\" href=\"schemars/trait.JsonSchema.html\" title=\"trait schemars::JsonSchema\">JsonSchema</a> for <a class=\"struct\" href=\"newtype_uuid/struct.TypedUuid.html\" title=\"struct newtype_uuid::TypedUuid\">TypedUuid</a>&lt;T&gt;<span class=\"where fmt-newline\">where\n    T: <a class=\"trait\" href=\"newtype_uuid/trait.TypedUuidKind.html\" title=\"trait newtype_uuid::TypedUuidKind\">TypedUuidKind</a> + <a class=\"trait\" href=\"schemars/trait.JsonSchema.html\" title=\"trait schemars::JsonSchema\">JsonSchema</a>,</span>"]],
+"schemars":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
